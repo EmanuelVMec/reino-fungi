846a02a5e533679fb0286533448b7cd687ae1859
EmanuelVMec <joshua.vinces5503@utc.edu.ec>
2025-07-17 22:53:09 -0500

    GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import { GoogleAnalytics } from '@next/third-parties/google'; // Usa este paquete oficial
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <GoogleAnalytics gaId="G-FT41T539QW" /> {/* Tu ID aquí */}
       </body>
     </html>
   );
